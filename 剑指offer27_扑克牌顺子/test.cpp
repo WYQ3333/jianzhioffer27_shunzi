4e2b00b0bed17062886f804664cc07d42414effd
浅忆梦微凉 <3538922590@qq.com>
2019-02-06 18:41:47 +0800

    目测通过

--- 剑指offer27_扑克牌顺子/test.cpp
+++ 剑指offer27_扑克牌顺子/test.cpp
@@ -23,7 +23,7 @@ public:
 				array.push_back(numbers[i]);
 				index++;
 			}
-			else if (numbers[i] - 1 == array[index]){
+			else if ((numbers[i] - 1 == array[index])){
 				array.push_back(numbers[i]);
 				index++;
 			}
@@ -34,7 +34,8 @@ public:
 				}
 				else{
 					while (cur){
-						array.push_back(0);
+						int temp = array[index];
+						array.push_back(++temp);
 						index++;
 						cur--;
 					}
