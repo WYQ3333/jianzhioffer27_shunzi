d0d3905046f6a96947839312b97ced8051c8ced5
浅忆梦微凉 <3538922590@qq.com>
2019-02-06 18:11:21 +0800

    编译通过

---
+++ 剑指offer27_扑克牌顺子/test.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+#include<vector>
+#include<algorithm>
+
+
+using namespace std;
+
+class Solution {
+public:
+	bool IsContinuous(vector<int> numbers) {
+		sort(numbers.begin(), numbers.end());
+		int new_size = numbers.size();
+		vector<int> array(new_size, 0);
+	}
+};
+
+void TestFunc(){
+
+}
+
+int main(){
+	TestFunc();
+	system("pause");
+	return 0;
+}
