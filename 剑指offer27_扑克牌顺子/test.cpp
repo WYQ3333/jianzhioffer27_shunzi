75686d7c2baa01646383cd4274a5705ae3015595
浅忆梦微凉 <3538922590@qq.com>
2019-02-06 18:25:46 +0800

    内存访问越界

--- 剑指offer27_扑克牌顺子/test.cpp
+++ 剑指offer27_扑克牌顺子/test.cpp
@@ -10,8 +10,39 @@ class Solution {
 public:
 	bool IsContinuous(vector<int> numbers) {
 		sort(numbers.begin(), numbers.end());
-		int new_size = numbers.size();
-		vector<int> array(new_size, 0);
+		vector<int> array;
+		int i = 0;
+		int num_zero = 0;
+		int index = 0;
+		while (i < numbers.size()){
+			if (numbers[i] == 0){
+				++num_zero;
+			}
+			if (numbers[i] != 0){
+				if (array.empty()){
+					array.push_back(numbers[i]);
+					index++;
+				}
+				else if (numbers[i] - 1 == array[index]){
+					array.push_back(numbers[i]);
+					index++;
+				}
+				else{
+					int cur = numbers[i] - array[index];
+					if (cur > num_zero){
+						return false;
+					}
+					else{
+						while (cur){
+							array.push_back(0);
+							index++;
+							cur--;
+						}
+					}
+				}
+			}
+			++i;
+		}
 		return true;
 	}
 };
